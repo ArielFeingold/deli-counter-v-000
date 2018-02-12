56139d8dcbce5c453c01112c3453872ed73b7755
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:40:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,12 @@
 katz_deli = []
 
 def line(name)
-  if katz_deli.count == 0
-    # name_array = []
-    # katz_deli.each_with_index do |name, index|
-    #   name_array << "#{index}. #{name}"
-    # end
-    puts "The line is currently empty."
-  elsif katz_deli.count == 3
-    puts "people"
+  if katz_deli.count != 0
+    name_array = []
+    katz_deli.each_with_index do |name, index|
+      name_array << "#{index}. #{name}"
+    end
+    puts "The line is currently: #{name_array}"
+  else puts "The line is currently empty."
   end
 end
