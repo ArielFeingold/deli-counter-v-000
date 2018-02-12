f3541d9f59259638348f482d8aafe8e54622e5c3
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:37:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,13 @@
 katz_deli = []
 
 def line(name)
-  if katz_deli.count != 0
+  if katz_deli.count == 0
     # name_array = []
     # katz_deli.each_with_index do |name, index|
     #   name_array << "#{index}. #{name}"
     # end
-    puts name_array
-  else puts "The line is currently empty."
+    puts "The line is currently empty."
+  else
+    puts "people"
   end
 end
