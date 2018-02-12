4253a090ff85fb03138d248375cd5922d06c7631
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:19:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@
 katz_deli = []
 
 def line(name)
-  if katz_deli.count == 0
+  if katz_deli.count == []
     puts "The line is currently empty."
   elsif katz_deli.count > 0
-    puts katz_deli 
+    puts katz_deli
   end
 end
