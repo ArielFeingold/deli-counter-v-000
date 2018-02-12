2ba7aa2f3ab4ab61875b32718533e96ce38f4522
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:36:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(name)
   if katz_deli.count != 0
-    name_array = []
+    # name_array = []
     # katz_deli.each_with_index do |name, index|
     #   name_array << "#{index}. #{name}"
     # end
