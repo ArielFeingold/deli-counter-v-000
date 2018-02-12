03d18874b3f269508ffcc4bcceb276b679a2e7c6
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:36:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ katz_deli = []
 def line(name)
   if katz_deli.count != 0
     name_array = []
-    katz_deli.each_with_index do |name, index|
-      name_array << "#{index}. #{name}"
-    end
+    # katz_deli.each_with_index do |name, index|
+    #   name_array << "#{index}. #{name}"
+    # end
     puts name_array
   else puts "The line is currently empty."
   end
