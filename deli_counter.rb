0e0c83badbb955f86369827880685d7fce2eb7a2
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:58:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,9 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+  end
+end
