0fc5365373dda13061ea2500151771cef00b7b4d
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 21:43:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       name_array << "#{index+1}. #{name}"
     end
-    puts "The line is currently: #{name_array}"
+    puts "The line is currently: #{name_array.strip}"
   else puts "The line is currently empty."
   end
 end
